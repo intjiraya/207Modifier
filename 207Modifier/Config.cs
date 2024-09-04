11e46f7fa2232c0c120b48d11c1323b63dd793d9
Jiraya <177346249+intjiraya@users.noreply.github.com>
2024-09-04 05:26:48 +0300

    first commit

---
+++ 207Modifier/Config.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace _207Modifier
+{
+    public class Config : IConfig
+    {
+        [Description("Is the plugin enabled?")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Enable debug mode?")]
+        public bool Debug { get; set; } = false;
+
+        [Description("Enable custom damage?")]
+        public bool CustomDamage { get; set; } = false;
+
+        [Description("Custom damage value (set to 0 to disable damage)")]
+        public float Damage { get; set; } = 0f;
+    }
+}
---
+++ 207Modifier/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+
+namespace _207Modifier
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "Jiraya";
+        public override string Name { get; } = "207Modifier";
+        public override string Prefix { get; } = "207Modifier";
+
+        public override System.Version Version { get; } = new System.Version(1, 0, 0);
+        public override System.Version RequiredExiledVersion { get; } = new System.Version(8, 9, 11);
+        public override PluginPriority Priority { get; } = PluginPriority.Medium;
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Hurting += OnHurting;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Hurting -= OnHurting;
+            base.OnDisabled();
+        }
+
+        private void OnHurting(HurtingEventArgs ev)
+        {
+            if (ev.DamageHandler.Type != DamageType.Scp207 || !Config.CustomDamage) return;
+            var scp207EffectsCount = ev.Player.ActiveEffects.Count(effect => effect.GetEffectType() == EffectType.Scp207);
+
+            if (scp207EffectsCount > 0)
+            {
+                ev.Amount = Config.Damage * scp207EffectsCount;
+            }
+        }
+    }
+}
